9b76b220e72c89869c8391811364168b2b9a942d
Marinela <marinelatogoe@gmail.com>
2022-05-17 22:54:05 +0200

    Applied functionality in order to abort launches in launches.model.js and launches.controller.js

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,7 +3,6 @@ const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launches = new Map();
 
 let latestFlightNumber = 100;
 
@@ -20,8 +19,10 @@ const launch = {
 
 saveLaunch(launch);
 
-function existsLaunchWithId(launchId) {
-    return launches.has(launchId);
+async function existsLaunchWithId(launchId) {
+    return await launchesDatabase.findOne({
+      flightNumber: launchId,
+    })
 }
 
 async function getLatestFlightNumber() {
@@ -73,11 +74,18 @@ async function scheduleNewLaunch(launch) {
 };
 
 
-function abortLaunchById(launchId) {
-    const aborted = launches.get(launchId);
-    aborted.upcoming = false;
-    aborted.success = false;
-    return aborted;
+async function abortLaunchById(launchId) {
+   const aborted = await launchesDatabase.updateOne({
+      flightNumber: launchId,
+
+    }, {
+      upcoming: false,
+      success: false,
+
+    });
+
+    return aborted.modifiedCount === 1;
+
 }
 
 module.exports = {
