05c9cb9275a475fb85f3c3b78e79b2a4479081dd
Marinela <marinelatogoe@gmail.com>
2022-05-18 16:43:02 +0200

    Added functionality for updating tests in Mongoose in side the server side package.json, services/mongo.js, launches.test.js, server.js

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,18 @@
 const request = require("supertest");
 const app = require("../../app");
-const { mongoConnect } = require("../../services/mongo");
+const {
+  mongoConnect,
+  mongoDisconnect,
+ } = require("../../services/mongo");
 
 
 describe("Launches API", () => {
   beforeAll(async () => {
     await mongoConnect();
+  });
+
+  afterAll(async () => {
+    await mongoDisconnect();
   });
 
   describe("Test GET /launches", () => {
@@ -21,7 +28,7 @@ describe("Launches API", () => {
     const completeLaunchData = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186 f",
+      target: "Kepler-62 f",
       launchDate: "January 4, 2028",
 
     };
@@ -29,13 +36,13 @@ describe("Launches API", () => {
     const launchDataWithoutDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186 f",
+      target: "Kepler-62 f",
     };
 
     const launchDataWithInvalidDate = {
       mission: "USS Enterprise",
       rocket: "NCC 1701-D",
-      target: "Kepler-186 f",
+      target: "Kepler-62 f",
       launchDate: "zoot",
     };
 
