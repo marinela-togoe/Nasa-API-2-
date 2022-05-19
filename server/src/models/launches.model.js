af018abefa114756128416b1aebc047c48d5f887
Marinela <marinelatogoe@gmail.com>
2022-05-19 20:19:16 +0200

    Created functionality inside server.js through .flatMap() method to map SpaceX Data ininto the Database

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -23,7 +23,7 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
-async function loadLaunchesData() {
+async function loadLaunchData() {
   console.log("Downloading launch data...");
   const response = await axios.post(SPACEX_API_URL, {
     query: {},
@@ -45,6 +45,26 @@ async function loadLaunchesData() {
     }
 
   });
+
+  const launchDocs = response.data.docs;
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc["payloads"];
+    const customers = payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      launchDate: launchDoc["date_local"],
+      upcoming: launchDoc["upcoming"],
+      success: launchDoc["success"],
+      customers,
+    };
+
+    console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
 }
 
 async function existsLaunchWithId(launchId) {
@@ -117,7 +137,7 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
-    loadLaunchesData,
+    loadLaunchData,
     existsLaunchWithId,
     getAllLaunches,
     scheduleNewLaunch,
