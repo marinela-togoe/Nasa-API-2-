8ffbfb559bc8ec997d1f19c4c882075d2defeaef
Marinela <marinelatogoe@gmail.com>
2022-05-23 20:31:08 +0200

    Installed dotenv npm package, added dotenv file in order to secure the MongoDB token and password, required the dotenv configuration inside server.js and mongo.js for testing purposes

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const MONGO_URL = "mongodb+srv://Marinela:JCrbDQofZocctdJL@nasacluster.zqqka.mongodb.net/nasa?retryWrites=true&w=majority";
+require("dotenv").config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 
 mongoose.connection.once("open", () => {
