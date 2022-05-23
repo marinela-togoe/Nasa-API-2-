803e8778088fcb4bca8685dfd39602a1cd7b87b1
Marinela <marinelatogoe@gmail.com>
2022-05-23 18:16:13 +0200

    Second attempt for Paginating Endpoints

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -112,11 +112,11 @@ if (!latestLaunch) {
    return latestLaunch.flightNumber;
 }
 
- async function getAllLaunches() {
+ async function getAllLaunches(skip, limit) {
     return await launchesDatabase
     .find({}, { "_id": 0, "__v": 0 })
-    .skip(20)
-    .limit(50);
+    .skip(skip)
+    .limit(limit);
 }
 
  async function saveLaunch(launch) {
